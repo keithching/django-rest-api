272ef746f6902cbaf284596b9b79bcb1007d3f62
Keith <keithching91@gmail.com>
2022-08-23 21:38:18 +0900

    fix: timezone set to tokyo; add date reg field to event provider model

--- backend/home/urls.py
+++ backend/home/urls.py
@@ -34,7 +34,7 @@ router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- backend/spark/models.py
+++ backend/spark/models.py
@@ -2,10 +2,13 @@ from django.db import models
 from pygments.lexers import get_all_lexers
 from pygments.styles import get_all_styles
 
+from django.utils import timezone
+
 class EventProvider(models.Model):
     # Primary keys (IDs) are added automatically. (You can override this, too.)
     name = models.CharField(max_length=32, unique=True, blank=False)
     email = models.CharField(max_length=32, unique=True, blank=False)
+    date_reg = models.DateTimeField('date registered', default=timezone.now)
     # password not to be saved in the DB
 
     def __str__(self): # string representation for this model
